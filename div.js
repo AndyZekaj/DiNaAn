0e5e4d3632fd8d944a6a09ff317f46bb9ad4ad28
Nataliia <nataliia.lishchynska@dci-student.org>
2024-11-05 13:46:42 +0100

    sending my updates on other taskst

--- div.js
+++ div.js
@@ -20,3 +20,5 @@ function concat(...args) {
 console.log(concat([1, 2, 3], [4, 5], [6, 7])); // [1, 2, 3, 4, 5, 6, 7]
 
 console.log(concat([1], [2], [3], [4], [5], [6], [7])); // [1, 2, 3, 4, 5, 6, 7]
+
+//Hi, Divya, Nata was here)))
