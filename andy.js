1e9f7e4be7a21c4062196b6d112251b6cc0eb509
Avdyl <avdyl.zekaj@dci-student.org>
2024-11-05 13:37:13 +0100

    initial commit

---
+++ andy.js
@@ -0,0 +1,8 @@
+const removeVowels = (string) => {
+    return string.replace(/[AEIOUaeiou]/g, ""); // "" Replace vowels with nothing
+  };
+  
+  console.log(removeVowels("table")); // "tbl"
+  console.log(removeVowels("JavaScript")); // "JvScrpt"
+  console.log(removeVowels("AEIOU")); // ""
+  console.log(removeVowels("ABCDe")); // "BCD"
