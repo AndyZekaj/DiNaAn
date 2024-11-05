45c6d64ec899e653cf0898ff398831088c56be8b
Divya <divya.raphy@dci-student.org>
2024-11-05 15:07:46 +0100

    test

--- NatDoc.js
+++ NatDoc.js
@@ -11,6 +11,7 @@ console.log(getVoteCount({ upvotes: 13, downvotes: 0 }))// ➞ 13
 console.log(getVoteCount({ upvotes: 2, downvotes: 33 }))// ➞ -31
 console.log(getVoteCount({ upvotes: 132, downvotes: 132 }))// ➞ 0
 
+console.log(getVoteCount({ upvotes: 133, downvotes: 40 }))
 
 //Divya Edited
 
